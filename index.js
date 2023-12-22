b6bd7cf77464c3c43774dbe03a1fca6be8b10cc3
void-the-droid <bhuwan626282@gmail.com>
2023-12-22 14:42:53 +0545

    aba break point ko tension baki rayo

---
+++ index.js
@@ -0,0 +1,78 @@
+gsap.from(".profile-box",{
+    x:-400,
+    duration:2,
+    delay:0.5,
+    scale:2,
+    opacity:0,
+
+})
+
+gsap.from(".img-box",{
+    x:-400,
+    duration:2,
+    delay:0.5,
+    scale:2,
+    opacity:0,
+
+})
+gsap.from("h2",{
+    x:800,
+    duration:2,
+    delay:0.5,
+    opacity:0,
+    
+})
+
+gsap.from("h1",{
+    x:800,
+    duration:2,
+    delay:0.5,
+    opacity:0,
+})
+
+gsap.from("button",{
+    x:800,
+    duration:2,
+    delay:2,
+    opacity:0,
+    stagger:1
+})
+
+gsap.from(".line",{
+    x:900,
+    duration:2,
+    delay:0.5,
+    rotate:720,
+    scale:1.5,
+})
+
+gsap.from(".social",{
+    y:50,
+    duration:2,
+    delay:0.5,
+    opacity:0,
+    scale:1.5,
+})
+
+gsap.from(".aside ",{
+    x:200,
+    duration:2,
+    delay:0.5,
+    opacity:0,
+    scale:1.5,
+})
+gsap.from(".black-box",{
+    x:200,
+    duration:2,
+    delay:0.5,
+    opacity:0,
+    scale:1.5,
+})
+
+gsap.from(".learn",{
+    x:200,
+    duration:2,
+    delay:0.5,
+    opacity:0.5,
+    scale:1.5,
+})
